3d0c486c217d075677c964117582b4d2ae01bd69
GenzCot <153144639+GenzCot@users.noreply.github.com>
2023-12-07 08:15:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>WhatsAap</title>
+</head>
+<body>
+
+     <div class="container">
+        <table class="table table-bordered border-primary">
+            <p>Nama : Gentra Ramadhann</p>
+            <p>NIM  : 231011401349</p>
+            <p>Kelas: 01TPLP033</p>
+            <p>RUANG: 305</p>
+            <h1>Table Kebenaran</h1>
+            <thead class="text-center ">
+                <tr>
+                    <td>No</td>
+                    <td>A</td>
+                    <td>B</td>
+                    <td>C</td>
+                    <td>NAND</td>
+                    <td>NOR</td>
+                    <td>AND</td>
+                    <td>OR</td>
+                </tr>
+            </thead>
+            <tbody class="text-center">
+                <tr>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            </tbody>
+
+        </table>
+     </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
